40faaa98af46b2b3ac3ff3d1d7b64007adf3b98e
AntoJusDeBagarre <156210053+AntoJusDeBagarre@users.noreply.github.com>
2025-05-21 16:46:02 +0200

    Vagrant

---
+++ setup (2)/client/app.py
@@ -0,0 +1,18 @@
+# k8s-app/app.py
+from flask import Flask
+from http import HTTPStatus
+from werkzeug.exceptions import HTTPException
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello Kubernetes!\n"
+
+@app.errorhandler(HTTPException)
+def handle_http_exception(exception: HTTPException):
+    return exception.description, exception.code
+
+@app.errorhandler(Exception)
+def handle_exception(exception: Exception):
+    return HTTPStatus.INTERNAL_SERVER_ERROR.description, HTTPStatus.INTERNAL_SERVER_ERROR
